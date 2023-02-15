6b7cf947eba552ed1c799cfa9faf714c5d248921
Lauren Waller <laurenhamann3@gmail.com>
2023-02-14 23:47:11 -0600

    Fix error

---
+++ src/hooks/blog-hub.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Layout from "../components/layout";
+import useBlogs from "./use-blogs";
+import { Link } from "gatsby";
+import { SmallIntro } from "../components/small-intro";
+import { Contact } from "../components/contact";
+const BlogHub = () => {
+  const posts = useBlogs();
+  return (
+    <Layout>
+      <SmallIntro />
+      <h1 id="blog-hub-title">Blog Hub</h1>
+      <section id="blog-hub-section">
+        {posts.map((p, i) => {
+          const title = p.title;
+          const date = p.date;
+          const key = i;
+          const slug = p.slug;
+          return (
+            <Link to={slug} key={key}>
+              <div className="blog-link">
+                <h2>{title}</h2>
+                <small>{date}</small>
+              </div>
+            </Link>
+          );
+        })}
+      </section>
+      <Contact />
+    </Layout>
+  );
+};
+
+export default BlogHub;
