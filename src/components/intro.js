b7e5c0729c9283ee37d07f2db56c95f90773202a
Lauren Waller <laurenhamann3@gmail.com>
2023-03-10 10:47:14 -0600

    change title image and update about

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,8 +1,14 @@
 import React from "react";
+import daisy from "../images/daisy.svg";
+
 export const Intro = () => {
   return (
     <header className="header" data-test="header">
-      <h1 id="title"> Lauren Hamann </h1>
+      <div className="flex-title">
+        <img src={daisy} />
+        <h1 id="title"> Lauren Hamann </h1>
+        <img src={daisy} />
+      </div>
       <p id="subtitle"> Frontend Developer </p>
       <div></div>
     </header>
